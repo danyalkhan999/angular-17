298d64ddf06dd9520c073b2a5f80ce2e0fe5571d
Danyal Khan <danyal.khan@ongraph.com>
2024-02-09 18:48:00 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CourseCardComponent } from './course-card/course-card.component';
+import { Course } from './Modal/course';
+import { COURSES } from './db-data';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CourseCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-17';
+  title = 'angular-17 module';
+  course: Course[] = [];
+
+  ngCourse: Course = COURSES[0];
+  rxjsCourse: Course = COURSES[1];
+  ngxCourse: Course = COURSES[2];
+
+  onkeyUp(text: string){
+    this.title = text
+  }
+
+  onCourseSelected(course: any){
+    console.log("button clicked app ", course);
+  }
 }
---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Course } from '../Modal/course';
+
+
+@Component({
+  selector: 'app-course-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './course-card.component.html',
+  styleUrl: './course-card.component.css'
+})
+export class CourseCardComponent {
+    @Input({
+      required:true // updated: now we can set required as true, so that if there is data require in rendering of any component, you must add it.
+    }) course!:Course;
+
+    @Output('onCourseSelected') selectedCourse = new EventEmitter<Course>();
+
+    viewCourse(){
+      console.log("button clickedd !!");
+      this.selectedCourse.emit(this.course)
+    }
+}
