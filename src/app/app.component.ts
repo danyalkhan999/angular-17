58a84e5a796b0483bf34e8baa0aa7f9965338ec7
Danyal Khan <danyal.khan@ongraph.com>
2024-02-13 13:14:44 +0530

    viewChild and viewChildren

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CourseCardComponent } from './course-card/course-card.component';
 import { Course } from './Modal/course';
@@ -12,17 +12,35 @@ import { CommonModule } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'angular-17 module';
   courses: Course[] = COURSES;
 
+  // @ViewChild('cardRef', {read: ElementRef}) card!: ElementRef;
+  @ViewChildren(CourseCardComponent)
+  cards!: QueryList<CourseCardComponent>;
+
   date: Date = new Date();
+
+  @ViewChild('container')
+  container!: ElementRef
+
+  constructor() {
+    console.log("container Div", this.cards);
+  }
+  ngAfterViewInit(): void {
+    this.cards.forEach(card => console.log(card))
+    // please avoid data modification here.
+    // this.courses[0].description = "test"
+  }
 
   onkeyUp(text: string){
     this.title = text
   }
 
   onCourseSelected(course: any){
-    console.log("button clicked app ", course);
+    console.log("button clicked app ", this.cards);
+
+    console.log("container", this.container);
   }
 }
